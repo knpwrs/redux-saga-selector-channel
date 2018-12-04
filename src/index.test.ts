14d315c2859b4cf4db3704fd8960b42e90cc33c9
Kenneth Powers <ken@kenpowers.net>
2018-12-03 21:56:49 -0500

    feat: initial implementation

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,68 @@
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
 import mod from '.';
 
 test('interface', () => {
-  expect(mod).toBe('Hello, World!');
+  expect(mod instanceof Function).toBe(true);
+});
+
+test('functionality', () => {
+  const sagaMiddleware = createSagaMiddleware();
+
+  const defaultState = {
+    runs: 0,
+    counter: 0,
+  };
+
+  const reducer = jest.fn((state: typeof defaultState = defaultState, action) => {
+    if (action.type === 'inc') {
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    }
+    if (action.type === 'inc-run') {
+      return {
+        ...state,
+        runs: state.runs + 1,
+      };
+    }
+    return state;
+  });
+
+  const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+  function* channelSaga(counter: number) {
+    const { runs: oldRuns } = store.getState();
+    expect(counter).toBe(store.getState().counter);
+    yield put({ type: 'inc-run' });
+    expect(store.getState().runs).toBe(oldRuns + 1);
+    expect(counter).toBe(store.getState().counter);
+  }
+
+  sagaMiddleware.run(function* testSaga() {
+    const chan = yield mod((state: typeof defaultState) => state.counter);
+    yield takeEvery(chan, channelSaga);
+  });
+
+  expect(store.getState()).toEqual(defaultState);
+  store.dispatch({ type: 'inc' });
+  expect(store.getState()).toEqual({
+    runs: 1,
+    counter: 1,
+  });
+  expect(reducer).toBeCalledTimes(3);
+  store.dispatch({ type: 'inc-run' });
+  expect(store.getState()).toEqual({
+    runs: 2,
+    counter: 1,
+  });
+  expect(reducer).toBeCalledTimes(4);
+  store.dispatch({ type: 'inc' });
+  expect(store.getState()).toEqual({
+    runs: 3,
+    counter: 2,
+  });
+  expect(reducer).toBeCalledTimes(6);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-export default 'Hello, World!';
+import {
+  Buffer,
+  channel,
+} from 'redux-saga';
+import {
+  CallEffect,
+  call,
+  put,
+  select,
+  spawn,
+  take,
+} from 'redux-saga/effects';
+
+export type StateSelector<T, U> = (state: T) => U;
+
+export default <T, U>(
+  selector: StateSelector<T, U>,
+  buffer?: Buffer<U>,
+): CallEffect => call(function* createSelectorChannel() {
+  const chan = channel<U>(buffer);
+  let previous: U;
+
+  yield spawn(function* runSelectorChannel() {
+    try {
+      while (true) {
+        yield take('*');
+        const result = yield select(selector);
+        if (result !== previous) {
+          yield put(chan, result);
+          previous = result;
+        }
+      }
+    } finally {
+      chan.close();
+    }
+  });
+
+  return chan;
+});
